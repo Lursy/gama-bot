96d9aed543f554cfce141bd5d3c38ee2cb2f2d0e
Lursy <70035797+Lursy@users.noreply.github.com>
2022-01-12 15:57:39 -0300

    Update image.py

--- Imagens/image.py
+++ Imagens/image.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def imagens_pericia(valor, pericia, resultado, nome):
-    calibriz_font = r'C:\Windows\Fonts\calibriz.ttf'
+    calibriz_font = r'CALIBRIZ.TTF'
     imagem = Image.open('Imagens/dado.png')
     font_num = ImageFont.truetype(calibriz_font, 55 if len(valor) < 2 else 110//len(valor))
     font_value = ImageFont.truetype(calibriz_font, 30)
