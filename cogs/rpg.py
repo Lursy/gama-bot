35632876c4d19816cd0d327408a0376de9989da4
Lursy <70035797+Lursy@users.noreply.github.com>
2022-02-14 22:53:50 -0300

    Update rpg.py

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 from Imagens import image
 import discord
 from random import randint
+
+
 def resultado(p, v, d=20):
     normal = d+1 - p
     bom = d+1 - p//2
@@ -17,19 +19,22 @@ def resultado(p, v, d=20):
     if v >= extremo:
         response = 'Extremo'
     return response
+
+
 class RPG(commands.Cog):
     """Dados para rpg"""
+
     def __init__(self, bot):
         self.bot = bot
+
     @commands.command(msg='dado')
     async def dado(self, ctx, pericia='', dado='20'):
         name = f'{ctx.author.name}'
         dado = int(dado.strip('d'))
         valor = randint(1, dado)
-        if name.lower() != 'jgkill' and name.lower() != 'big chungus':
+        if name.lower() != 'big chungus':
             valor2 = randint(1, dado)
             if valor2 > valor:
-                print(valor2, valor)
                 valor = valor2
         prc = rst = ''
         nome = f'Nome: {name}'
@@ -39,6 +44,7 @@ class RPG(commands.Cog):
             rst = f'Resultado: {resul}'
         image.imagens_pericia(nome=nome, valor=str(valor), pericia=prc, resultado=rst)
         await ctx.send(file=discord.File(r'Imagens/resultado.png'))
+
     @commands.command(msg='lista')
     async def lista(self, ctx, tam='20'):
         d = int(tam)
@@ -52,55 +58,71 @@ class RPG(commands.Cog):
             extremo = d + 1 - p // 5
             string += f'{p:<4}- N:{normal:<4} B:{bom:<4} E:{extremo:<4}\n'
         await ctx.send(string)
+
     @commands.command(msg='dano')
-    async def dano(self, ctx, *, dados):
+    async def dano(self, ctx, *, dano):
+
+        card = discord.Embed(color=0x000000)
         try:
-            line = '━━━━━━━━━━━━━━━━━━━━━'
-            dx = dados.replace(' ', '').split('+')
-            vd = va = resultado = ''
-            adicional = {}
-            dado = {}
-            dano = 0
-            for item in dx:
-                if item.isnumeric():
-                    adicional['adicional: '] = item
-                else:
-                    print('foi')
-                    if 'd' in item.casefold():
-                        valores = item.split('d')
-                        for c in range(0, int(valores[0])):
-                            dado[f'{c+1}° d{valores[1]} = '] = str(randint(1, int(valores[1])))
-            card = discord.Embed(color=0x000000)
-            for key, value in dado.items():
-                if len(dado) == 1:
-                    vd = key + value + '\n'
-                    dano += int(value)
-                    resultado = value
-                else:
-                    vd += f'{key+value}\n'
-                    dano += int(value)
-            if len(dado) > 1:
-                resultado = ' + '.join(dado.values())
-            vd += line
-            card.add_field(name=f'Rolagem: {dados}', value=vd, inline=False)
-            if adicional:
-                resultado += ' + '
-                for key, value in adicional.items():
-                    if len(adicional) == 1:
-                        va += key + value
-                        dano += int(value)
+            import random
+
+            rolagem = dano
+            val = {}
+            sinais = {}
+            cont = 0
+            dano = dano.replace(' ', '')
+
+            for c, item in enumerate(dano):
+                if not item.isalnum():
+                    if c != 0:
+                        cont += 1
+                        dano = dano.replace(item, ',')
                     else:
-                        va += f'{key + value}\n'
-                        dano += int(value)
-                if len(dado) > 1:
-                    resultado += ' + '.join(adicional.values())
-                va += '\n' + line
-                card.add_field(name=f'Adicionais', value=va, inline=False)
-            resultado = f'{resultado} = {dano}'
-            card.add_field(name=f'Resultado', value=resultado, inline=False)
-            card.set_footer(text='Distribuição GaMa', icon_url=self.bot.user.avatar_url)
+                        dano = dano[1:]
+                    sinais[cont] = item
+
+            dano = dano.split(',')
+
+            for c, item in enumerate(dano):
+                if 'd' in item:
+                    dado = item.split('d')
+                    for cont in range(0, int(dado[0])):
+                        if cont == 0:
+                            dano.pop(c)
+                            val[c] = [str(random.randint(1, int(dado[1])))]
+                        else:
+                            val[c].append(str(random.randint(1, int(dado[1]))))
+
+            tam = len(dano)
+
+            for key, value in val.items():
+                if len(dano) != tam:
+                    key += 1
+                    tam = len(dano)
+                dano.insert(key, str(eval('+'.join((value)))))
+
+            tam = len(dano)
+            cont = 1
+
+            for k, v in sinais.items():
+                if len(dano) != tam:
+                    k += cont
+                    cont += 1
+                    tam = len(dano)
+                dano.insert(k, v)
+
+            vals = ''
+            for item in val.values():
+                for c, v in enumerate(item):
+                    vals += f'{c+1}° dado = {v}\n'
+
+            response = f'\nResultado: {eval(" ".join(dano))}'
+            card.add_field(name=f'Dados na mesa! Rolagem: {rolagem}', value=response, inline=False)
+            card.add_field(name=f'Valores', value=vals, inline=False)
             await ctx.send(embed=card)
         except Exception as error:
-            await ctx.send(f'O parâmetro "{dados}" não foi aceito\nErro: "{error}"')
+            await ctx.send(f'O parâmetro "{dano}" não foi aceito\nErro: "{error}"')
+
+
 def setup(bot):
     bot.add_cog(RPG(bot))
