c7fe1674ce755fa707cc14f03b22b70fac6423bb
Lursy <70035797+Lursy@users.noreply.github.com>
2022-02-16 09:46:10 -0300

    Update rpg.py

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -109,9 +109,9 @@ class RPG(commands.Cog):
                     k += cont
                     cont += 1
                     tam = len(dano)
-                dano.insert(k, v)
+                dano.insert(k, v.replace('*', '×'))
 
-            card.add_field(name=f'Dados na mesa!', value=f"{rolagem}\n"+"".join(dano).replace('*', '×') + f' = {eval(" ".join(dano))}', inline=False)
+            card.add_field(name=f'Dados na mesa!', value=f"{rolagem}\n"+"".join(dano) + f' = {eval(" ".join(dano))}', inline=False)
             await ctx.send(embed=card)
         except Exception as error:
             await ctx.send(f'O parâmetro "{dano}" não foi aceito\nErro: "{error}"')
